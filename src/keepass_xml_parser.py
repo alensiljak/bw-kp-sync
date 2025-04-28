688a18f7ea358aa8001ff756f49987dc1247ac22
alensiljak <462445+alensiljak@users.noreply.github.com>
2025-04-28 17:47:32 +0200

    reading xml

--- src/keepass_xml_parser.py
+++ src/keepass_xml_parser.py
@@ -1,6 +1,7 @@
 '''
     Parser for KeePass XML file export.
 '''
+import xml.etree.ElementTree as ET
 
 class KeePassXmlParser:
     '''
@@ -16,6 +17,14 @@ class KeePassXmlParser:
         '''
         Read KeePass XML file.
         '''
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             data = file.read()
         return data
+
+    def read_xml_file(self, file_path):
+        '''
+        Read KeePass XML file and parse it.
+        '''
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        return root
--- tests/test_kp.py
+++ tests/test_kp.py
@@ -2,21 +2,28 @@
 Test reading KeePass XML export.
 '''
 
+import os
 from src.keepass_xml_parser import KeePassXmlParser
+import xml.etree.ElementTree as ET
 
 
 class TestKeePassXmlParser:
     '''
     Test KeePass XML Parser.
     '''
-    def __init__(self):
+    def setup_class(self):
+        '''
+        Setup class for KeePass XML Parser tests.
+        '''
         self.kp = KeePassXmlParser()
+        self.home_dir = os.path.expanduser("~")
+        self.file_path = os.path.join(self.home_dir, 'desktop', 'kp.xml')
 
     def test_read_file(self):
         '''
         Test reading KeePass XML file.
         '''
-        contents = self.kp.read_file('c:/users/alen/desktop/kp.xml')
+        contents = self.kp.read_file(self.file_path)
 
         assert contents is not None, 'File contents should not be None'
         assert isinstance(contents, str), 'File contents should be a string'
@@ -24,3 +31,36 @@ class TestKeePassXmlParser:
         assert '<KeePassFile>' in contents, 'File contents should contain <KeePassFile>'
         assert '</KeePassFile>' in contents, 'File contents should contain </KeePassFile>'
         assert '<Group>' in contents, 'File contents should contain <Group>'
+        assert '</Group>' in contents, 'File contents should contain </Group>'
+        assert '<Entry>' in contents, 'File contents should contain <Entry>'
+        assert '</Entry>' in contents, 'File contents should contain </Entry>'
+        # assert '<String>' in contents, 'File contents should contain <String>'
+        # assert '</String>' in contents, 'File contents should contain </String>'
+        # assert '<Title>' in contents, 'File contents should contain <Title>'
+        # assert '</Title>' in contents, 'File contents should contain </Title>'
+        # assert '<UserName>' in contents, 'File contents should contain <UserName>'
+        # assert '</UserName>' in contents, 'File contents should contain </UserName>'
+        # assert '<Password>' in contents, 'File contents should contain <Password>'
+        # assert '</Password>' in contents, 'File contents should contain </Password>'
+
+    def test_read_first_item(self):
+        '''
+        Test reading first item from KeePass XML file.
+        '''
+        root = self.kp.read_xml_file(self.file_path)
+
+        assert root is not None, 'File contents should not be None'
+        assert isinstance(root, ET.Element), 'Parsed XML should be an Element'
+        assert len(root) > 0, 'Parsed XML should not be empty'
+
+        # print(ET.tostring(xml, encoding='unicode'))
+        # item = xml.find('Entry')
+        entry = root.find('.//Entry')
+        if entry is not None:
+            print('Entry:', entry.tag, entry.attrib, entry.text)
+
+        assert entry is not None, 'Entry should not be None'
+        assert entry.tag == 'Entry', 'Entry tag should be <Entry>'
+        assert len(entry) > 0, 'Entry should not be empty'
+        # assert 'UUID' in entry.attrib, 'Entry should have UUID attribute'
+        assert False
